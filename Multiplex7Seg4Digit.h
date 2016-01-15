8642bcbc932adaf15843859177291032804829cd
AndreyU <ushakov.andrey.34@gmail.com>
2016-01-15 16:47:02 +0100

    Changed wiring.

--- Multiplex7Seg4Digit.h
+++ Multiplex7Seg4Digit.h
@@ -3,20 +3,20 @@
 
 
 #define DIG_1  0b00000010
-#define DIG_2  0b00000100
-#define DIG_3  0b00001000
-#define DIG_4  0b00010000
-
-#define ZERO   0b11000000
-#define ONE    0b11111001
-#define TWO    0b10100100
-#define THREE  0b10110000
-#define FOUR   0b10011001
-#define FIVE   0b10010010
-#define SIX    0b10000010
-#define SEVEN  0b11111000
-#define EIGHT  0b10000000
-#define NINE   0b10010000
+#define DIG_2  0b00010000
+#define DIG_3  0b00100000
+#define DIG_4  0b01000000
+			     
+#define ZERO   0b00101000
+#define ONE    0b11101101
+#define TWO    0b00110100
+#define THREE  0b10100100
+#define FOUR   0b11100001
+#define FIVE   0b10100010
+#define SIX    0b00100010
+#define SEVEN  0b11101100
+#define EIGHT  0b00100000
+#define NINE   0b10100000
 
 
 class Multiplex7Seg4Digit {
